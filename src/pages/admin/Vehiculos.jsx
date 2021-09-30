5e8ebee0bf4b3158beab96e8e59cc340218d9eef
Carlos Rojas <caranroja@outlook.com>
2021-09-29 22:08:37 -0500

    agregar datos a la tabla cuando se agegar vehiculo y salga registro exitoso

--- src/pages/admin/Vehiculos.jsx
+++ src/pages/admin/Vehiculos.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const vehiculosBack = [//Simula el back end
     {
@@ -48,24 +50,61 @@ const Vehiculos = () => {
     return (
         <div className='flex flex-col items-center justify-center w-full'>
             <h2 className='text-3xl'>Pagina admin vehiculos</h2>
-            <button onClick={()=>{setMostrarTabla(!mostrarTabla)}} className={`text-white bg-${colorBoton}-500 p-5' type='button`}>{textBoton}</button>
+            <button onClick={()=>{setMostrarTabla(!mostrarTabla)}} className={`text-white bg-${colorBoton}-500 p-4 m-3 rounded-md`} type='button'>{textBoton}</button>
             {
-                mostrarTabla ? (<TablaVehiculos listaVehiculos={vehiculos}/>):(<FormularioVehiculos/>)//establece los vehiculos en la tabla
-            }            
+                mostrarTabla ? (<TablaVehiculos listaVehiculos={vehiculos}/>)
+                :(<FormularioVehiculos 
+                    funcionMostrarTabla={setMostrarTabla}
+                    listaVehiculos={vehiculos} 
+                    funcionAgregarVehiculo={setVehiculos}/>)//establece los vehiculos en la tabla
+            }
+            <ToastContainer position="bottom-center" autoClose={3000}/>            
         </div>              
     )
 }
 
-const FormularioVehiculos = () => {
+const FormularioVehiculos = ({funcionMostrarTabla, listaVehiculos, funcionAgregarVehiculo}) => {
+    const [nombre, setNombre] = useState();
+    const [marca, setMarca] = useState();
+    const [modelo, setModelo] = useState();
+
+    const enviarAlBackend = () => {
+        console.log('nombre', nombre, 'marca', marca, 'modelo', modelo)
+        toast.success("Registro exitoso")
+        funcionMostrarTabla(true);
+        funcionAgregarVehiculo([...listaVehiculos, {nombre:nombre, marca:marca, modelo:modelo}])
+    }
+
     return (
     <div>
         <h2 className='text-3xl text-center'>Crear vehiculo</h2>
-        <form className='grid grid-cols-2'>
-        <input className='bg-gray-300 focus-within:outline-none rounded-md p-2 mx-2 my-2' type="text" />
-        <input className='bg-gray-300 focus-within:outline-none rounded-md p-2 mx-2 my-2' type="text" />
-        <input className='bg-gray-300 focus-within:outline-none rounded-md p-2 mx-2 my-2' type="text" />
-        <input className='bg-gray-300 focus-within:outline-none rounded-md p-2 mx-2 my-2' type="text" />
-        <button className='col-span-2 bg-gray-500 focus-within:outline-none p-3 my-1' type='button'>Guardar vehiculo</button>
+        <form className='flex flex-col'>
+        <label className="flex flex-col items-center" htmlFor="nombre">
+            nombre vehiculo
+            <input 
+            value={nombre} 
+            onChange={(e)=>{setNombre(e.target.value)}} name="nombre" className='bg-gray-300 focus-within:outline-none rounded-md p-2 mx-2 my-2' type="text" />
+        </label>
+        <label className="flex flex-col items-center" htmlFor="marca">
+            nombre marca
+            <select 
+            value={marca} 
+            onChange={(e)=>{setMarca(e.target.value)}} className='bg-gray-300 focus-within:outline-none rounded-md p-2 mx-2 my-2' name="marca">
+                <option disabled>Seleccione una opcion</option>
+                <option>Ford</option>
+                <option>Chevrolet</option>
+                <option>Mazda</option>
+            </select>
+        </label>
+        <label className="flex flex-col items-center" htmlFor="modelo">
+            modelo
+            <input 
+            value={modelo} 
+            onChange={(e)=>{setModelo(e.target.value)}} name="modelo" className='bg-gray-300 focus-within:outline-none rounded-md p-2 mx-2 my-2' type="number" min={1990} max={2022} />
+        </label>   
+        <button 
+        onClick={()=>{enviarAlBackend()}}
+        className='col-span-2 bg-gray-500 focus-within:outline-none p-3 my-1' type='button'>Guardar vehiculo</button>
         </form>
     </div>
     )
