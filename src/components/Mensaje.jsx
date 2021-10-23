665115492b0c059918e8b481663dc07c4962d06f
Carlos Rojas <caranroja@outlook.com>
2021-10-22 19:06:33 -0500

    Se agregan botones al index para hacer una contador con renderizado condicional para decir si es par o impar

---
+++ src/components/Mensaje.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Mensaje = ({contadorInicial}) => {
+    return (
+        <h1>{contadorInicial}</h1>
+    )
+}
+
+export default Mensaje
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Index = () => {
+
+    const [contador, setContador] = useState(0)
+
+    const esPar = (contador % 2 === 0)
+
     return (
-        <div>Contenido landing de la pagina</div>
+        <div className='mx-4'>
+            <div>Contenido landing de la pagina</div>
+            <button 
+            className="bg-gray-700 rounded-lg px-4 text-red-50 transform hover:scale-125 mr-4 shadow-lg"
+            onClick={()=>{setContador(contador+1)}}>
+                click
+            </button>
+            <button 
+            className="bg-gray-700 rounded-lg px-4 text-red-50 transform hover:scale-125 shadow-lg"
+            onClick={()=>{setContador(0)}}>
+                reset
+            </button>
+            <div>{contador}</div>
+            <small>{esPar?'Es par':'Es impar'}</small>
+        </div>
     )
 }
 
